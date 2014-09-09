6092fc295c0a95174c61b3a88b524f2fed54a6b5
sciencepro <d@dcarr.io>
2014-09-09 03:30:14 -0700

    add some sample cards

--- app/src/main/java/breakingnews/dcarr/io/breakingnews/BreakingNewsActivity.java
+++ app/src/main/java/breakingnews/dcarr/io/breakingnews/BreakingNewsActivity.java
@@ -1,29 +1,151 @@
 package breakingnews.dcarr.io.breakingnews;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class BreakingNewsActivity extends Activity {
+
+    private String authToken = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breaking_news);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+
+
+        AccountManager mgr = AccountManager.get(this);
+        Account[] accts = mgr.getAccountsByType("com.google");
+        Account acct = accts[0];
+        AccountManagerFuture<Bundle> accountManagerFuture = mgr.getAuthToken(acct, Constants.SCOPE, null, this, null, null);
+        Bundle authTokenBundle = null;
+        try {
+            authTokenBundle = accountManagerFuture.getResult();
+        } catch (OperationCanceledException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (AuthenticatorException e) {
+            e.printStackTrace();
         }
+        authToken = authTokenBundle.get(AccountManager.KEY_AUTHTOKEN).toString();
+
+        RelativeLayout cardOne = (RelativeLayout) findViewById(R.id.cardOne);
+        TextView cardOneName = (TextView) cardOne.findViewById(R.id.list_widget_name);
+        cardOneName.setText("US Open");
+        TextView cardOneDesc = (TextView) cardOne.findViewById(R.id.list_widget_description);
+        cardOneDesc.setText("Marin Cilic of Croatia wins US Open in straight sets over Kei Nishikori");
+        cardOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    generateTimelineCard(Constants.cardThree);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        RelativeLayout cardTwo = (RelativeLayout) findViewById(R.id.cardTwo);
+        TextView cardTwoName = (TextView) cardOne.findViewById(R.id.list_widget_name);
+        cardTwoName.setText("Home Depot Breach");
+        TextView cardTwoDesc = (TextView) cardOne.findViewById(R.id.list_widget_description);
+        cardTwoDesc.setText("Home Depot confirms its payment systems were breached; no evidence PIN numbers compromised");
+        cardTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    generateTimelineCard(Constants.cardTwo);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        RelativeLayout cardThree = (RelativeLayout) findViewById(R.id.cardThree);
+        TextView cardThreeName = (TextView) cardOne.findViewById(R.id.list_widget_name);
+        cardThreeName.setText("Penn Footabll");
+        TextView cardThreeDesc = (TextView) cardOne.findViewById(R.id.list_widget_description);
+        cardThreeDesc.setText("NCAA restores Penn State's eligibility for football bowl games, scholarships");
+        cardThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    generateTimelineCard(Constants.cardThree);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+    private void generateTimelineCard(String cardData) throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("html", cardData);
+        JSONArray menuItmes = new JSONArray();
+
+        JSONObject deleteMenu = new JSONObject();
+        deleteMenu.put("action", "DELETE");
+
+        menuItmes.put(deleteMenu);
+
+        json.put("menuItems", menuItmes);
+
+        JSONObject notification = new JSONObject();
+        notification.put("level", "DEFAULT");
+
+        json.put("notification", notification);
+
+        MirrorApiClient.getInstance(BreakingNewsActivity.this).createTimelineItem(authToken, json, new MirrorApiClient.Callback() {
+            @Override
+            public void onSuccess(HttpResponse response) {
+                Log.d("mirror", "success");
+            }
+
+            @Override
+            public void onFailure(HttpResponse response, Throwable e) {
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                    StringBuilder builder = new StringBuilder();
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        builder.append(line);
+                        builder.append("\n");
+                    }
+                    Log.e("mirror response", builder.toString());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                Log.e("mirror", "error");
+            }
+        });
     }
 
 
@@ -44,21 +166,5 @@ public class BreakingNewsActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_breaking_news, container, false);
-            return rootView;
-        }
     }
 }
--- app/src/main/java/breakingnews/dcarr/io/breakingnews/Constants.java
+++ app/src/main/java/breakingnews/dcarr/io/breakingnews/Constants.java
@@ -7,6 +7,57 @@ import org.json.JSONObject;
  */
 public class Constants {
 
-    public static String scope = "oauth2:https://www.googleapis.com/auth/glass.timeline https://www.googleapis.com/auth/glass.location https://www.googleapis.com/auth/plus.login";
+    public static final String SCOPE = "oauth2:https://www.googleapis.com/auth/glass.timeline https://www.googleapis.com/auth/glass.location https://www.googleapis.com/auth/plus.login";
+
+    public static final String cardOne = "<article class=\"author\">\n" +
+            "  <img src=\"https://docs.google.com/a/scienceprousa.com/uc?id=0B7hkyM_drsvOdG9Xcmc3aEZqc0E&amp;export=download\" width=\"100%\" height=\"100%\" />\n" +
+            "  <div class=\"overlay-full\"></div>\n" +
+            "  <header>\n" +
+            "    <img src=\"https://lh6.googleusercontent.com/-qgpapQjmHEc/AAAAAAAAAAI/AAAAAAABU3o/bUQx3qc5zhM/w800-h800/photo.jpg\" />\n" +
+            "    <h1>US Open</h1>\n" +
+            "    <h2>CBS Sports</h2>\n" +
+            "  </header>\n" +
+            "  <section>\n" +
+            "    <p class=\"text-auto-size\">Marin Cilic of Croatia wins US Open in straight sets over Kei Nishikori</p>\n" +
+            "  </section>\n" +
+            "  <footer>\n" +
+            "    <p style=\"float: left\">BreakingNews</p>\n" +
+            "    <time>5:35</time>\n" +
+            "  </footer>\n" +
+            "</article>";
+
+    public static final String cardTwo = "<article class=\"author\">\n" +
+            "  <img src=\"https://docs.google.com/a/scienceprousa.com/uc?id=0B7hkyM_drsvObzhuYmtuX3Z0bjA&amp;export=download\" width=\"100%\" height=\"100%\" />\n" +
+            "  <div class=\"overlay-full\"></div>\n" +
+            "  <header>\n" +
+            "    <img src=\"https://lh6.googleusercontent.com/-qgpapQjmHEc/AAAAAAAAAAI/AAAAAAABU3o/bUQx3qc5zhM/w800-h800/photo.jpg\" />\n" +
+            "    <h1>Home Depot Breach</h1>\n" +
+            "    <h2>CNBC</h2>\n" +
+            "  </header>\n" +
+            "  <section>\n" +
+            "    <p class=\"text-auto-size\">Home Depot confirms its payment systems were breached; no evidence PIN numbers compromised</p>\n" +
+            "  </section>\n" +
+            "  <footer>\n" +
+            "    <p style=\"float: left\">Breaking News</p>\n" +
+            "    <time>5:39</time>\n" +
+            "  </footer>\n" +
+            "</article>";
+
+    public static final String cardThree = "<article class=\"author\">\n" +
+            "  <img src=\"https://docs.google.com/a/scienceprousa.com/uc?id=0B7hkyM_drsvORmZTdE4wSFhQWm8&amp;export=download\" width=\"100%\" height=\"100%\" />\n" +
+            "  <div class=\"overlay-full\"></div>\n" +
+            "  <header>\n" +
+            "    <img src=\"https://docs.google.com/a/scienceprousa.com/uc?id=0BysO7O2z9sluaEZnN2dvTmtfZnM&amp;export=download\" />\n" +
+            "    <h1>Penn Football</h1>\n" +
+            "    <h2>NCAA</h2>\n" +
+            "  </header>\n" +
+            "  <section>\n" +
+            "    <p class=\"text-auto-size\">NCAA restores Penn State&#39;s eligibility for football bowl games, scholarships</p>\n" +
+            "  </section>\n" +
+            "  <footer>\n" +
+            "    <p style=\"float: left\">BreakingNews</p>\n" +
+            "    <time>6:09</time>\n" +
+            "  </footer>\n" +
+            "</article>";
 
 }
