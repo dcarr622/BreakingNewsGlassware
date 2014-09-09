dc7f3726dbf3063ecb540348570395bfc8acdde1
sciencepro <dcarr@scienceprousa.com>
2014-09-09 02:41:53 -0700

    initial commit

---
+++ app/src/main/java/breakingnews/dcarr/io/breakingnews/Constants.java
@@ -0,0 +1,12 @@
+package breakingnews.dcarr.io.breakingnews;
+
+import org.json.JSONObject;
+
+/**
+ * Created by david on 9/8/14.
+ */
+public class Constants {
+
+    public static String scope = "oauth2:https://www.googleapis.com/auth/glass.timeline https://www.googleapis.com/auth/glass.location https://www.googleapis.com/auth/plus.login";
+
+}
